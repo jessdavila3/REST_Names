762f6a593e3d4b36abb73bc2f751249114e81457
Jesse Davila <jessedavila3@gmail.com>
2017-03-27 16:51:34 -0500

    cleaned up controller

--- src/main/java/hello/PersonController.java
+++ src/main/java/hello/PersonController.java
@@ -1,10 +1,13 @@
 package hello;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.ServletRequestDataBinder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
